15793d871eb2bc874b2781d2381ae144ff9dc209
PhilipJose21 <gadexjign@gmail.com>
2026-06-05 15:27:20 +0800

    Fixed Merge

--- Assets/Scripts/HighlightTile.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-
-public class HighlightTile : MonoBehaviour
-{
-    public Vector3 worldPosition;
-    public bool isMove;
-    public bool isAttack;
-}
--- Assets/Scripts/UnitSOContainer.cs
+++
@@ -1,6 +0,0 @@
-using UnityEngine;
-
-public class UnitSOContainer : MonoBehaviour
-{
-    public UnitSO unitData;
-}
