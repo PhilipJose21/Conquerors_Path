d7e7ffff1ac27228bd67590a202114c2e77fbb27
unknown <xbpadaoan@gmail.com>
2026-06-22 12:04:42 +0800

    added Research Tree Scene, scroll view

---
+++ Assets/Scripts/UI/ResearchTree/ResearchNodeData.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "NewResearchNode", menuName = "Research/Node")]
+public class ResearchNodeData : ScriptableObject
+{
+    [Header("UI Display Settings")]
+    public string nodeId;
+    public string nodeDisplayName;
+    [TextArea(3, 5)] public string description;
+    public int cost;
+
+    [Header("Path Prerequisites")]
+    public bool isUnlockedByDefault = false;
+    public ResearchNodeData[] prerequisites;
+
+    [Header("Game Modifiers")]
+    public UpgradeType targetUpgrade;
+    public float modifierValue;
+
+    public enum UpgradeType
+    {
+        None,
+        SupportOutput,
+        SupportHP,
+        RangerRange,
+        RangerDamage,
+        ScoutMobility,
+        MeleeDamage,
+        MeleeHP,
+        MeleeMobility
+    }
+}
