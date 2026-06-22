9ff69dab23eac463963d3692a704a30f8c2e25dd
PhilipJose21 <gadexjign@gmail.com>
2026-06-23 07:46:51 +0800

    Success merge

--- Assets/Scripts/Player/PlayerData.cs
+++ Assets/Scripts/Player/PlayerData.cs
@@ -2,12 +2,29 @@ using UnityEngine;
 
 public class PlayerData : MonoBehaviour
 {
+    public static PlayerData Instance { get; private set; }
+
+    [Header("Core Scriptable Object References")]
     public PlayerSO playerSO;
     public PlayerBattleSO playerBattleSO;
+
     public int playerWood;
     public int playerStone;
     public int playerFarm;
     public int playerGold;
+
+    void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
 
     void Update()
     {
