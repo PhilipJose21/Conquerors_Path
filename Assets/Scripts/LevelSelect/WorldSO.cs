6b68b5a99290c944659fabce658c8d56a68c4e58
Xaviery Bonson Padaoan <xbpadaoan@gmail.com>
2026-07-10 03:49:45 +0800

    Level Select: Settings, UI, and battle units list dropdown

--- Assets/Scripts/LevelSelect/WorldSO.cs
+++ Assets/Scripts/LevelSelect/WorldSO.cs
@@ -6,6 +6,7 @@ using System.Collections.Generic;
 [CreateAssetMenu()]
 public class WorldSO : ScriptableObject
 {
+    public Color worldBackgroundColor = Color.white;
     public string worldName;
     public Sprite worldImage;
     public string worldLevelScene;
